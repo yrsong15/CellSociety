096441e5ddc14d8bdc453e8d966f16e8cbcaf26d
Chalena <cm296@duke.edu>
2016-09-24 17:15:07 -0400

    fixed bug: removing species before neighbors can use them as a reference to make their move

--- src/util/GameEngine.java
+++ src/util/GameEngine.java
@@ -1,35 +1,45 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import species.Species;
 import util.Grid;
 
 /***
- *
-
  * @author Chalena
- *
  */
 public class GameEngine {
-	
+
+
 	public void updateWorld(Grid myGrid){
-		Grid copyGrid = new Grid(myGrid.myGrid, myGrid.getWidth(), myGrid.getHeight());
+		Grid copyGrid = new Grid(myGrid.myGrid, myGrid.getWidth(), myGrid.getHeight(), myGrid.getNeighbType());
+		List<Location> toDelete = new ArrayList<Location>();
 		for (int i = 0; i < myGrid.getWidth(); i++){
 			for (int j = 0; j < myGrid.getHeight(); j++){
 				Location currLoc = new Location(i, j);
 				Species currSpecies= myGrid.getCell(currLoc);
 				if (currSpecies != null){
 					Location moveTo = currSpecies.performTask(copyGrid.getEmptyCells(), copyGrid.getNeighborhood(currLoc));
-					if (moveTo != null){
-						myGrid.setCell(currLoc, null);
-						myGrid.setCell(moveTo, currSpecies);
-						
+					if (moveTo == null){
+						toDelete.add(currLoc);
+					}
+					else if(!moveTo.equals(currLoc)){
+						myGrid.setCell(currLoc, currSpecies);
 					}
 				}
 			}
 		}
+		clearFallenSpecies(toDelete, myGrid);
 		updateStates(myGrid);
 	}
 	
+	
+	private void clearFallenSpecies(List<Location> toDelete, Grid myGrid){
+		for (int i = 0; i < toDelete.size(); i++){
+			myGrid.setCell(toDelete.get(i), null);
+		}
+	}
 	
 	public void updateStates(Grid myGrid){
 		for (int i = 0; i < myGrid.getWidth(); i++){
