134ca675d770a3ddcc7f2749368ca6ab6aa3d428
Chalena <cm296@duke.edu>
2016-09-23 23:05:41 -0400

    adding predatorPrey subclass

---
+++ src/util/PredatorPrey.java
@@ -0,0 +1,48 @@
+package util;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import Species.Shark;
+import Species.Species;
+import Species.WatorSpecies;
+
+public class PredatorPrey extends Simulation{
+
+	public PredatorPrey() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Grid populateGrid() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Grid myGrid = new Grid(getGridHeight(), getGridWidth());
+	    NodeList nList = myXML.getElementsByTagName("species");
+        for (int temp = 0; temp < nList.getLength(); temp++) {//for each species
+	            Element speciesType = (Element) nList.item(temp);
+	            setProperties(speciesType, myGrid);
+        }
+      return myGrid;
+	}
+	
+	public void setProperties(Element speciesType, Grid myGrid) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Species mySpecies;
+        int percent = Integer.parseInt(speciesType.getElementsByTagName("percent").item(0).getTextContent());
+        int breedTurns = Integer.parseInt(speciesType.getElementsByTagName("breedTurns").item(0).getTextContent());
+        int numCells = Integer.parseInt(getElement("numCells"));
+        String type = speciesType.getAttribute("type");
+        int createNum = (int) Math.ceil((numCells*(percent/100.0)));//number need to create of species w/ this state
+        for (int created = 0; created < createNum; created++){
+        	mySpecies = createSpecies(type);
+        	if (type.equals("Shark")){
+        		int starveTurns = Integer.parseInt(speciesType.getElementsByTagName("starveTurns").item(0).getTextContent());
+        		((Shark) mySpecies).setMyStarveTime(starveTurns);
+        	}
+        	((WatorSpecies) mySpecies).setTimeuntilBreed(breedTurns);
+        	if (speciesAdded < numCells){
+        		myGrid.addCell((Species) mySpecies);
+        		speciesAdded++;
+        	}
+        }
+	}
+}
