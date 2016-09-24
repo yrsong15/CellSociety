572b80497dc8c67b1f94af759e268811d46969d1
Chalena <cm296@duke.edu>
2016-09-24 10:24:35 -0400

    updated super class to have general method of populateGrid instead of each subclass having it

--- src/simulation_config/SimulationConfig.java
+++ src/simulation_config/SimulationConfig.java
@@ -15,7 +15,6 @@ import util.Location;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
-import java.io.File;
 
 
 
@@ -46,17 +45,53 @@ public abstract class SimulationConfig {
      }
 	}
 	
+	
+	/**
+	 * 
+	 * @param parent parent tag of the tag to retrieve string value from
+	 * @param element name of the tag in the xml file
+	 * @return returns the string value content of the tag in the xml file
+	 */
+	public String getElement(Element parent, String tagName ){
+		return parent.getElementsByTagName(tagName).item(0).getTextContent();
+	}
+	
+	
 	/**
 	 * @param element name of the tag in the xml file
 	 * @return returns the string value content of the tag in the xml file
 	 */
-	public String getElement(String element){
-		return myXML.getElementsByTagName(element).item(0).getTextContent();
+	public String getElement(String tagName ){
+		return myXML.getElementsByTagName(tagName).item(0).getTextContent();
 	}
 	
 	
+	public abstract void setParameters(Element speciesInfo, Species mySpecies);
 	
-	public abstract Grid populateGrid() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException;
+	public Grid populateGrid(){		
+		Grid myGrid = new Grid(getGridHeight(), getGridWidth());
+	    NodeList speciesList = myXML.getElementsByTagName("species");
+	    int numCells = Integer.parseInt(getElement("numCells"));
+	    for (int curr = 0; curr < speciesList.getLength(); curr++) {//for each species
+            Element currSpecies= (Element) speciesList.item(curr);
+            String speciesType = currSpecies.getAttribute("type");	            
+            NodeList percentList = currSpecies.getElementsByTagName("percent");
+    	    for (int i = 0; i < percentList.getLength(); i++) {//for each percent of state in species
+	            int percent = Integer.parseInt(percentList.item(i).getTextContent());
+	            int createNum = (int) Math.ceil((numCells*(percent/100.0)));//number need to create of species w/ this state
+	            for (int created = 0; created < createNum; created++){
+	            	if (speciesAdded < numCells){
+		            	Species mySpecies = createSpecies(speciesType);
+		            	mySpecies.setCurrState(Integer.parseInt(((Element) percentList.item(i)).getAttribute("state")));
+		            	this.setParameters(currSpecies, mySpecies);
+	            		myGrid.addCell((Species) mySpecies);
+	            		speciesAdded++;
+	            	}	            	
+	            }
+    	    }
+	    }
+	    return myGrid;
+	}
 	
 	//handle these errors better; meaning catch them
 	public Grid populateGridTest() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
@@ -87,15 +122,21 @@ public abstract class SimulationConfig {
 		
 	}
 	
-	//to-do: handle errors
-	public Species createSpecies(String speciesType) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+	//to-do: handle errors better
+	public Species createSpecies(String speciesType) {
 		Species mySpecies = null;
 		try {
 			Class<?> speciesClass = Class.forName("species." + speciesType);
-			Constructor<?> constructor = speciesClass.getConstructor();
+			Constructor<?> constructor = null;
+			try {
+				constructor = speciesClass.getConstructor();
+			} catch (NoSuchMethodException | SecurityException e) {
+				e.printStackTrace();
+			}
 			mySpecies = (Species) constructor.newInstance();
 			
-		} catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			System.out.println("Species provided in XML file cannot be found, does not have a proper constructor, or is not a valid class");
 			e.printStackTrace();
 		} 
 		
