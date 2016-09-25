9a93434145e77f0bac76bcf0c5057c37502becf1
Chalena <cm296@duke.edu>
2016-09-25 16:09:35 -0400

    added cloning capabilities, keep track of visited species so don't revisit, and adjacent functions to location class

--- src/util/GameEngine.java
+++ src/util/GameEngine.java
@@ -15,17 +15,24 @@ public class GameEngine {
 	public void updateWorld(Grid myGrid){
 		Grid copyGrid = new Grid(myGrid.myGrid, myGrid.getWidth(), myGrid.getHeight(), myGrid.getNeighbType());
 		List<Location> toDelete = new ArrayList<Location>();
+		List<Location> emptyCells = copyGrid.getEmptyCells();
+		List<Species> alreadyVisited = new ArrayList<Species>();
 		for (int i = 0; i < myGrid.getWidth(); i++){
 			for (int j = 0; j < myGrid.getHeight(); j++){
 				Location currLoc = new Location(i, j);
-				Species currSpecies= copyGrid.getCell(currLoc);
-				if (currSpecies != null){
-					Location moveTo = currSpecies.performTask(myGrid.getEmptyCells(), copyGrid.getNeighborhood(currLoc));
+				Species currSpecies= myGrid.getCell(currLoc);
+				if (currSpecies != null && !alreadyVisited.contains(currSpecies)){
+					alreadyVisited.add(currSpecies);
+					Location moveTo = currSpecies.performTask(emptyCells, copyGrid.getNeighborhood(currLoc));
 					if (moveTo == null){
 						toDelete.add(currLoc);
 					}
 					else if(!moveTo.equals(currLoc)){
+						emptyCells.remove(moveTo);
 						move(currLoc, moveTo, currSpecies, myGrid);
+						if (currSpecies.toBreed()){
+							myGrid.setCell(currLoc, currSpecies.clone(currLoc));
+						}
 					}
 				}
 			}
--- src/util/Location.java
+++ src/util/Location.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Location {
 	private int x, y;
 	public Location(int x, int y){
@@ -25,5 +28,27 @@ public class Location {
 
 	public void setY(int y) {
 		this.y = y;
+	}
+	
+	//could add an isDiagonal method, this only checks for plus type adjacent
+	public boolean isAdjacent(Location pos){
+		if ((this.getX() == pos.getX()) && ((this.getY() == (pos.getY()-1)) || (this.getY() == (pos.getY()+1)))){
+			return true;	
+		}
+		else if ((this.getY() == pos.getY()) && ((this.getX() == (pos.getX()-1)) || this.getX() == (pos.getX()+1))){
+				return true;
+		}
+		return false;
+	}
+	
+	public List<Location> getAdjacentCells(List<Location> possible){
+		List<Location> adjacent = new ArrayList<Location>();
+		for (int i = 0; i < possible.size(); i++){
+			Location otherLoc = possible.get(i);
+			if (this.isAdjacent(otherLoc)){
+				adjacent.add(possible.get(i));
+			}
+		}
+		return adjacent;
 	}
 }
