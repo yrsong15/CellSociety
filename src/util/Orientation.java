441e24d580aa6746505ef8b2d380c753f816b1df
Chalena <cm296@duke.edu>
2016-09-30 20:51:17 -0400

    adding ant species and orientation classes

---
+++ src/util/Orientation.java
@@ -0,0 +1,16 @@
+package util;
+public class Orientation {
+	private String Orientation;
+	
+	
+	public Orientation(String orientation){
+		this.setOrientation(orientation);
+	}
+	
+	public String getOrientation() {
+		return Orientation;
+	}
+	public void setOrientation(String orientation) {
+		Orientation = orientation;
+	}
+}
